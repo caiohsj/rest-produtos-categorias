17091469a41a28c767d10797c99ce4aac1551fb6
unknown <caiocoxim10@gmail.com>
2019-09-25 11:58:20 -0400

    Listagem de produtos versão 2.0

--- src/components/ProdutoList.vue
+++ src/components/ProdutoList.vue
@@ -1,6 +1,30 @@
 <template>
     <div>
         <b-table striped hover :items="produtos" :fields="cabecalhos"></b-table>
+        <div class="row">
+            <b-card v-for="produto in produtos" v-bind:key="produto.id"
+                    img-src=""
+                    img-alt="Image"
+                    img-top
+                    tag="article"
+                    style="max-width: 20rem;"
+                    class="m-2"
+            >
+
+                <b-card-img :src="`data:image/png;base64,${produto.img}`">
+                </b-card-img>
+
+                <b-card-text class="font-weight-bold">
+                    {{produto.description}}
+                </b-card-text>
+
+                <b-card-text>Preço: R${{produto.price}}</b-card-text>
+                <b-card-text>Marca: {{produto.brand}}</b-card-text>
+
+                <b-button href="#" variant="primary" class="m-2">Visualizar</b-button>
+                <b-button href="#" variant="success">Add Carrinho</b-button>
+            </b-card>
+        </div>
     </div>
 </template>
 
@@ -28,7 +52,8 @@
                         key: 'category.description',
                         label: 'Categoria'
                     }
-                ]
+                ],
+                img: "aa"
             }
         }
     }
